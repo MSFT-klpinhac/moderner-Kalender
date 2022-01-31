d6c302d0fdbc32455af55abf45a7ac83826b158f
Dennis Klee <DennisKlee@bundeswehr.org>
2022-01-31 13:20:34 +0100

    Kalender hinzugefügt

--- src/webparts/gwBwKalender/components/GwBwKalender.tsx
+++ src/webparts/gwBwKalender/components/GwBwKalender.tsx
@@ -2,23 +2,34 @@ import * as React from 'react';
 import styles from './GwBwKalender.module.scss';
 import { IGwBwKalenderProps } from './IGwBwKalenderProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import BigCalendar from 'react-big-calendar'
+import * as moment from 'moment';
+
+
 
 export default class GwBwKalender extends React.Component < IGwBwKalenderProps, {} > {
+  
   public render(): React.ReactElement<IGwBwKalenderProps> {
+    let events = [ 
+      {
+        title: "Februar 2",
+      start: new Date("2022-02-02T16:24:00"),
+      end: new Date("2022-02-02T17:24:00"),
+    },{
+      title: "Februar 5",
+      start: new Date("2022-02-05T16:24:00"),
+      end: new Date("2022-02-07T17:24:00"),
+  }
+    ];
+    const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
     return(
       <div className = { styles.gwBwKalender } >
-  <div className={styles.container}>
-    <div className={styles.row}>
-      <div className={styles.column}>
-        <span className={styles.title}>Welcome to SharePoint!</span>
-        <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
-        <p className={styles.description}>{escape(this.props.description)}</p>
-        <a href='https://aka.ms/spfx' className={styles.button}>
-          <span className={styles.label}>Learn more</span>
-        </a>
-      </div>
-    </div>
-  </div>
+        <BigCalendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+    />
       </div >
     );
   }
